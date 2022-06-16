64153c274a9603b45425f5922d0a218a78249387
Zhangruhuan <zhangruhuan@126.com>
2022-06-16 15:29:43 +0800

    master commit2

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -6,6 +6,8 @@ public class GitTest {
         System.out.println("Hello Git2");
         System.out.println("Hello Git3");
         System.out.println("Hello Git4");
+        System.out.println("master test!");
+
 
 
     }
